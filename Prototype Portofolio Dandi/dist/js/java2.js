13e17da61edef8824900c55397439baf702a23ec
Baltazarx <144795383+Baltazarx@users.noreply.github.com>
2024-07-06 18:26:20 +0700

    Add files via upload

---
+++ Prototype Portofolio Dandi/dist/js/java2.js
@@ -0,0 +1,26 @@
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+  window.onload = function() {
+  window.scrollTo(0, 0);
+  }
+
+  var slideIndex = 1;
+  showDivs(slideIndex);
+
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
+  }
+
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    x[slideIndex-1].style.display = "block";  
+  }
